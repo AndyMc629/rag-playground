5d318a31122a9e4ddc6075525b1ad7761024797b
andymc629 <andrewpmcmahon629@gmail.com>
2024-08-31 08:46:48 +0000

    Base notebook starting to run, having networking issues to running postgres server (docker).

--- infra/dockerfiles/simple-pgvector/init_pgvector.sql
+++ infra/dockerfiles/simple-pgvector/init_pgvector.sql
@@ -1,2 +1,11 @@
 /* Create the vector extension */
 CREATE EXTENSION IF NOT EXISTS vector;
+
+/*
+CREATE TABLE IF NOT EXISTS embeddings (
+  id SERIAL PRIMARY KEY,
+  embedding vector,
+  text text,
+  created_at timestamptz DEFAULT now()
+);]
+*/
